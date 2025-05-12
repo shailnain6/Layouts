6b4167c58a07b17e06117ce0caf141eacaaf2094
Shailendra Nain <shailnain.6@gmail.com>
2025-05-11 21:43:33 -0400

    Layouts updated

--- Layouts/ContentView.swift
+++ Layouts/ContentView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = "A quick brown fox jumps over the lazy dog."
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundStyle(.orange)
+                .padding()
+                .frame(width: 350, height: 150)
+            
+            HStack {
+                Button("Awesome"){
+                    message = "You are Awesome!!"
+                }
+                Button("Great"){
+                    message = "You are Great!"
+                }
+            }
+            .buttonStyle(.borderedProminent)
         }
         .padding()
+        
     }
 }
 
